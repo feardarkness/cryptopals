3d12a085da4692508c8951e1d8f2c5b18097fa05
feardarkness <alvarado.ariel.contact@gmail.com>
2018-10-29 13:47:18 -0400

    added challenge one and two solutions

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Printf("Hi")
+	challengeOne()
+	challengeTwo()
+}
+
+func challengeOne() {
+	hexString := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+	hexDecoded := decodeFromHex([]byte(hexString))
+	base64Encoded := encodeToBase64([]byte(hexDecoded))
+	fmt.Println("--- Challenge One ---")
+	fmt.Printf("String to Base64, response and expected respectively: \n%s\n", base64Encoded)
+	fmt.Printf("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t\n\n")
+}
+
+func challengeTwo() {
+	buffer1 := decodeFromHex([]byte("1c0111001f010100061a024b53535009181c"))
+	buffer2 := decodeFromHex([]byte("686974207468652062756c6c277320657965"))
+	xordBuffers := xorSameLengthBuffers(buffer1, buffer2)
+	fmt.Println("--- Challenge Two ---")
+	fmt.Printf("Xord buffers, response and expected respectively: \n%s\n", encodeToHex(xordBuffers))
+	fmt.Println("746865206b696420646f6e277420706c6179\n\n")
 }
--- utils.go
+++ utils.go
@@ -1,6 +1,36 @@
 package main
 
-func toBase64(src []byte) byte {
-	dest := make([]byte, base64.EncodedLen(len(src)))
+import (
+	"encoding/base64"
+	"encoding/hex"
+	"log"
+)
+
+func encodeToBase64(src []byte) []byte {
+	dest := make([]byte, base64.StdEncoding.EncodedLen(len(src)))
+	base64.StdEncoding.Encode(dest, src)
 	return dest
+}
+
+func decodeFromHex(src []byte) []byte {
+	dest := make([]byte, hex.DecodedLen(len(src)))
+	_, err := hex.Decode(dest, src)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return dest
+}
+
+func encodeToHex(src []byte) []byte {
+	dest := make([]byte, hex.EncodedLen(len(src)))
+	hex.Encode(dest, src)
+	return dest
+}
+
+func xorSameLengthBuffers(buffer1 []byte, buffer2 []byte) []byte {
+	resultBuffer := make([]byte, len(buffer1))
+	for index := range buffer1 {
+		resultBuffer[index] = buffer1[index] ^ buffer2[index]
+	}
+	return resultBuffer
 }
